aa4960be33454ce1b05ab129e90758532da99f60
isaquemsf <zaque8069@gmail.com>
2025-06-09 14:59:50 -0300

    Exercicios 14 e 15 - Fuctura

---
+++ src/exercicios_introducao/basicos_II/FVogalOuConsoante.java
@@ -0,0 +1,16 @@
+package exercicios_introducao.basicos_II;
+
+import java.util.Scanner;
+
+public class FVogalOuConsoante {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite uma letra:");
+        String letra = scanner.next();
+
+        if (letra.equals("a") || letra.equals("A") || letra.equals("e") || letra.equals("E")){
+            
+        }
+    }
+}
