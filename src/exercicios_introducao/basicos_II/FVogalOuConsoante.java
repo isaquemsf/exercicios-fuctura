1dbf1089af9562f90d4a23a1a273c9d41ab78724
isaquemsf <zaque8069@gmail.com>
2025-06-09 21:43:26 -0300

    Exercicios 16,17,18,19,20 e 21 - Fuctura

--- src/exercicios_introducao/basicos_II/FVogalOuConsoante.java
+++ src/exercicios_introducao/basicos_II/FVogalOuConsoante.java
@@ -9,8 +9,25 @@ public class FVogalOuConsoante {
         System.out.println("Digite uma letra:");
         String letra = scanner.next();
 
-        if (letra.equals("a") || letra.equals("A") || letra.equals("e") || letra.equals("E")){
-            
+        if (letra.length() == 1) {
+            if (letra.equals("0") || letra.equals("1") || letra.equals("2") ||
+                    letra.equals("3") || letra.equals("4") || letra.equals("5") ||
+                    letra.equals("6") || letra.equals("7") || letra.equals("8") ||
+                    letra.equals("9")) {
+                System.out.println("Formato inválido, digite uma letra!");
+
+            } else if (letra.equalsIgnoreCase("a") ||
+                    letra.equalsIgnoreCase("e") ||
+                    letra.equalsIgnoreCase("i") ||
+                    letra.equalsIgnoreCase("o") ||
+                    letra.equalsIgnoreCase("u")) {
+                System.out.printf("A letra (%s) é uma vogal.", letra);
+
+            } else {
+                System.out.printf("A letra (%s) é uma consoante.", letra);
+            }
+        } else {
+            System.out.println("Digite apenas uma letra!");
         }
     }
 }
