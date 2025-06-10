bf8a9ce65146d86da6ff07f19b61d7adfae9d75d
isaquemsf <zaque8069@gmail.com>
2025-06-10 14:53:51 -0300

    Exercicios 28 e 29 - Fuctura

---
+++ src/exercicios_introducao/lacorepeticao/AContador.java
@@ -0,0 +1,9 @@
+package exercicios_introducao.lacorepeticao;
+
+public class AContador {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
